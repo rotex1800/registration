c2c90cdc2c0a7e09051708f9dde8ebcb52b3ccd0
Paul Schuberth <mail@paulschuberth.de>
2022-06-23 23:14:43 +0200

    Spring Initialzr

---
+++ src/main/kotlin/de/rotex1800/registration/RegistrationApplication.kt
@@ -0,0 +1,11 @@
+package de.rotex1800.registration
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RegistrationApplication
+
+fun main(args: Array<String>) {
+    runApplication<RegistrationApplication>(*args)
+}
---
+++ src/test/kotlin/de/rotex1800/registration/RegistrationApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.rotex1800.registration
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RegistrationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
