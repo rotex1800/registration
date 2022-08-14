5b25c7cb26fbbb886cfab0f502c175b3ffa23650
Paul Schuberth <mail@paulschuberth.de>
2022-08-14 20:14:27 +0200

    convert to pest test

--- tests/Feature/Livewire/EventSummaryTest.php
+++ tests/Feature/Livewire/EventSummaryTest.php
@@ -4,26 +4,16 @@ namespace Tests\Feature\Livewire;
 
 use App\Http\Livewire\EventSummary;
 use App\Models\Event;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Livewire\Livewire;
-use Tests\TestCase;
 
-class EventSummaryTest extends TestCase
-{
-    use DatabaseMigrations;
-
-    /** @test */
-    public function the_component_can_render()
-    {
-        $event = Event::factory()->create();
-        $component = Livewire::test(EventSummary::class, [
-            'event' => $event
-        ]);
-        $component
-            ->assertStatus(200)
-            ->assertSee($event->name)
-            ->assertSee($event->start->isoFormat('d. MMMM Y'))
-            ->assertSee($event->end->isoFormat('d. MMMM Y'))
-        ;
-    }
-}
+test('event summary can render', function () {
+    $event = Event::factory()->create();
+    $component = Livewire::test(EventSummary::class, [
+        'event' => $event
+    ]);
+    $component
+        ->assertStatus(200)
+        ->assertSee($event->name)
+        ->assertSee($event->start->isoFormat('d. MMMM Y'))
+        ->assertSee($event->end->isoFormat('d. MMMM Y'));
+});
