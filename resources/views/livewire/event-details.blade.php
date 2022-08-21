9e5fa67d61235a84de6295cb6e24b7e26add9d10
Paul Schuberth <mail@paulschuberth.de>
2022-08-21 22:19:31 +0200

    style buttons for registering and un-registering

--- resources/views/livewire/event-details.blade.php
+++ resources/views/livewire/event-details.blade.php
@@ -1,18 +1,22 @@
 <div>
     <div class="text-4xl"> {{ $this->event->name }}</div>
-    <div class="my-1 flex flex-row text-2xl">
-        <div class="mr-2">Von:</div>
-        <div>{{ $this->event->start->isoFormat('d. MMMM Y') }}</div>
+    <div class="flex flex-row">
+        <div>
+            <div class="my-1 flex flex-row text-2xl">
+                <div class="mr-2">Von:</div>
+                <div>{{ $this->event->start->isoFormat('d. MMMM Y') }}</div>
+            </div>
+            <div class="my-1 flex flex-row text-2xl">
+                <div class="mr-2">Bis:</div>
+                <div>{{ $this->event->end->isoFormat('d. MMMM Y') }}</div>
+            </div>
+        </div>
     </div>
-    <div class="my-1 flex flex-row text-2xl">
-        <div class="mr-2">Bis:</div>
-        <div>{{ $this->event->end->isoFormat('d. MMMM Y') }}</div>
-    </div>
-    <div class="my-1">
+    <div class="my-1 text-white ">
         @if($this->hasUserRegistered())
-            <button wire:click="unregister">Abmelden</button>
+            <button class="bg-yellow-500 p-3 rounded" wire:click="unregister">Abmelden</button>
         @else
-            <button wire:click="register">Anmelden</button>
+            <button class="bg-blue-800 p-3 rounded" wire:click="register">Anmelden</button>
         @endif
     </div>
 </div>
