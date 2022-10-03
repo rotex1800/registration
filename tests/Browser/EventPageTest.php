67660f7aca68e78057332567c9367b3908ad3f3e
Paul Schuberth <mail@paulschuberth.de>
2022-10-03 23:39:19 +0200

    Run browser tests in GitHub too

--- tests/Browser/EventPageTest.php
+++ tests/Browser/EventPageTest.php
@@ -3,6 +3,7 @@
 use App\Models\Event;
 use App\Models\Role;
 use App\Models\User;
+use Laravel\Dusk\Browser;
 use function Pest\Laravel\actingAs;
 
 it('shows main navigation', function () {
@@ -110,4 +111,11 @@ it('defaults to part one if unknown part defined', function () {
         ->get(route('event.show', $event).'?part=nonexistent')
         ->assertStatus(200)
         ->assertSee(__('registration.about-yeo'));
+});
+
+it('can access browser', function () {
+    $this->browse(function (Browser $browser) {
+        $browser->visit('/')
+                ->assertSee('Laravel');
+    });
 });
