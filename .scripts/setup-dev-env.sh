e2e5cdaa18f83923760bebc4003e3106c82810fa
Paul Schuberth <mail@paulschuberth.de>
2022-10-08 15:30:41 +0200

    Add script to prepare the dev environment
    Usage: Call from the root directory of the project!

---
+++ .scripts/setup-dev-env.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Update Chrome driver
+php artisan dusk:update
+
+# Detect Chrome Driver
+php artisan dusk:chrome-driver --detect
+
+# Start Chrome Driver
+.vendor/laravel/dusk/bin/chromedriver-mac-arm &
+
+# Install Node dependencies and run in dev mode
+npm install
+npm run dev &
+
+# Run laravel dev server
+php artisan serve &
