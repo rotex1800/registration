591faaa5c3a8e303509a8a8a24fba4a4f071e607
Paul Schuberth <mail@paulschuberth.de>
2022-10-09 21:52:27 +0200

    Add little explanation on welcome page

--- lang/de/root.php
+++ lang/de/root.php
@@ -1,6 +1,9 @@
 <?php
 
 return [
-    'introduction' => 'Deutschland Tour!',
-    'welcome' => 'Willkommen',
+    'welcome' => 'Hallo liebe:r Inbound!',
+    'introduction' => 'Es freut uns, dass du mit uns auf Tour fahren möchtest! Bitte registriere dich und
+     logge dich ein um mit der Anmeldung zu beginnen.',
+    'help' => 'Wenn du Fragen hast oder Hilfe brauchst, melde dich bitte bei uns!'
+
 ];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     <h1 class="text-4xl">{{ __('root.welcome') }}</h1>
     <p class="mt-2">{{ __('root.introduction') }}</p>
+    <p class="my-2">{{ __('root.help') }}</p>
 
-    <a class="inline-block bg-blue-800 rounded p-2 text-white font-semibold" href="{{ route('login') }}"
+    <a class="inline-block bg-blue-800 rounded p-2 text-white font-semibold mx-2" href="{{ route('login') }}"
        id="login">{{ __('signup.login') }}</a>
-
     <a class="inline-block bg-blue-800 rounded p-2 text-white font-semibold" href="{{ route('register') }}"
        id="register">{{ __('signup.register') }}</a>
 @endsection
