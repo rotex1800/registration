bfd6c5c23860eb9c6799531285a1dbfd33f54f42
Paul Schuberth <mail@paulschuberth.de>
2022-10-09 22:28:29 +0200

    Fix tests and apply pint formatting

--- lang/de/root.php
+++ lang/de/root.php
@@ -4,6 +4,5 @@ return [
     'welcome' => 'Hallo liebe:r Inbound!',
     'introduction' => 'Es freut uns, dass du mit uns auf Tour fahren möchtest! Bitte registriere dich und
      logge dich ein um mit der Anmeldung zu beginnen.',
-    'help' => 'Wenn du Fragen hast oder Hilfe brauchst, melde dich bitte bei uns!'
-
+    'help' => 'Wenn du Fragen hast oder Hilfe brauchst, melde dich bitte bei uns!',
 ];
