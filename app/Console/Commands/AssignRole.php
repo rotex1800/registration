d2860f391f4896fafefdf649b0b182b8d808b236
Paul Schuberth <mail@paulschuberth.de>
2022-10-13 00:17:00 +0200

    Artisan command to assign a role to a user

---
+++ app/Console/Commands/AssignRole.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class AssignRole extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'registration:assign {role?} {email?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Assign a role to a user';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $role = $this->argument('role');
+        if ($role == null) {
+            $role = $this->ask("What's the name of the role?");
+        }
+
+        $email = $this->argument('email');
+        if ($email == null) {
+            $email = $this->ask("What's the email of the user?");
+        }
+
+        $user = User::where('email', $email)->first();
+        if ($user == null) {
+            $this->error("There is no user with email '$email'");
+            return Command::FAILURE;
+        }
+        $this->info("Assigning role '$role' to user with email '$email'");
+        $user->giveRole($role);
+
+        return Command::SUCCESS;
+    }
+}
---
+++ tests/Feature/Console/Commands/AssignRoleTest.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+test('command works', function () {
+    $user = User::factory()->create();
+    $role = fake()->word;
+    $this->artisan('registration:assign', [
+        'role' => $role,
+        'email' => $user->email
+    ])->assertExitCode(0);
+    expect($user->hasRole($role))->toBeTrue();
+});
+
+it('fails if user can not be found', function () {
+    $role = fake()->word;
+    $email = fake()->email;
+    $this->artisan('registration:assign', [
+        'role' => $role,
+        'email' => $email
+    ])->assertFailed();
+});
+
+it('it asks for role and user when called without arguments', function () {
+    $user = User::factory()->create();
+    $this->artisan('registration:assign')
+         ->expectsQuestion("What's the name of the role?", 'rotex')
+         ->expectsQuestion("What's the email of the user?", $user->email)
+         ->expectsOutput("Assigning role 'rotex' to user with email '$user->email'")
+         ->assertSuccessful();
+    expect($user->hasRole('rotex'))->toBeTrue();
+});
+
+it('shows error message if the user can not be found', function () {
+    $email = fake()->email;
+    $this->artisan('registration:assign', [
+        'email' => $email,
+        'role' => fake()->word
+    ])->expectsOutput("There is no user with email '$email'")
+         ->assertFailed();
+});
