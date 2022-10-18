89261093e6f7a6a349837c62eb68dc80400f50d2
Paul Schuberth <mail@paulschuberth.de>
2022-10-19 01:39:59 +0200

    remove duplicate import

--- database/factories/HostFamilyFactory.php
+++ database/factories/HostFamilyFactory.php
@@ -2,8 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\HostFamily;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /** @extends Factory<HostFamily> */
