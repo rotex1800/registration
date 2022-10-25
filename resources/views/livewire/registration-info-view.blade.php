94c96a0a6a984472964c03c9fc5a13913a26e810
Paul Schuberth <mail@paulschuberth.de>
2022-10-25 23:18:43 +0200

    show information about currently selected attendee

--- resources/views/livewire/registration-info-view.blade.php
+++ resources/views/livewire/registration-info-view.blade.php
@@ -1,7 +1,79 @@
 <div>
-    <select>
+    <select wire:model="currentAttendeeId">
         @foreach($attendees as $attendee)
             <option value="{{ $attendee->id }}">{!! $attendee->full_name !!}</option>
         @endforeach
     </select>
+
+    <div class="text-lg">
+
+        <div>
+            <div>{{ __('registration.birthday').': '.($currentAttendee?->birthday?->translatedFormat('d. F Y') ?: '--') }}</div>
+            <div>{{ __('registration.gender.gender').': '.($currentAttendee?->gender ?: '--') }}</div>
+            <div>{{ __('signup.email').': '.($currentAttendee?->email ?: '--') }}</div>
+            <div>{{ __('registration.mobile_phone').': '.($currentAttendee?->mobile_phone ?: '--') }}</div>
+            <div>{{ __('registration.health_issues').': '.($currentAttendee?->health_issues ?: '--') }}</div>
+        </div>
+        <div>
+            {{ __('registration.passport') }}
+            <div>{{ __('registration.nationality').': '.($currentAttendee?->passport?->nationality ?: '--') }}</div>
+            <div>{{ __('registration.passport-number').': '.($currentAttendee?->passport?->passport_number ?: '--') }}</div>
+            <div>{{ __('registration.passport-issue-date').': '.($currentAttendee?->passport?->issue_date ?: '--') }}</div>
+            <div>{{ __('registration.passport-expiration-date').': '.($currentAttendee?->passport?->expiration_date ?: '--') }}</div>
+        </div>
+
+        <div>
+            {{ __('registration.about-rotary') }}
+            <div>{{ __('registration.rotary.host-club').': '.($currentAttendee?->rotaryInfo?->host_club ?: '--') }}</div>
+            <div>{{ __('registration.rotary.host-district').': '.($currentAttendee?->rotaryInfo?->host_district ?: '--') }}</div>
+            <div>{{ __('registration.rotary.sponsor-club').': '.($currentAttendee?->rotaryInfo?->sponsor_club ?: '--') }}</div>
+            <div>{{ __('registration.rotary.sponsor-district').': '.($currentAttendee?->rotaryInfo?->sponsor_district ?: '--') }}</div>
+        </div>
+
+        <div>
+            {{ __('registration.about-counselor') }}
+            <div>{{ __('registration.counselor.name').': '.($currentAttendee?->counselor?->name ?: '--') }}</div>
+            <div>{{ __('registration.counselor.telephone').': '.($currentAttendee?->counselor?->phone ?: '--') }}</div>
+            <div>{{ __('registration.counselor.email').': '.($currentAttendee?->counselor?->email?: '--') }}</div>
+        </div>
+
+        <div>
+            {{ __('registration.about-yeo') }}
+            <div>{{ __('registration.yeo.name').': '.($currentAttendee?->yeo?->name ?: '--') }}</div>
+            <div>{{ __('registration.yeo.telephone').': '.($currentAttendee?->yeo?->phone ?: '--') }}</div>
+            <div>{{ __('registration.yeo.email').': '.($currentAttendee?->yeo?->email?: '--') }}</div>
+        </div>
+
+        <div>
+            {{ __('registration.about-bio-family') }}
+            <div>{{ __('registration.bio-family.parent-one').': '.($currentAttendee?->bioFamily?->parent_one ?: '--') }}</div>
+            <div>{{ __('registration.bio-family.parent-two').': '.($currentAttendee?->bioFamily?->parent_two ?: '--') }}</div>
+            <div>{{ __('registration.bio-family.email').': '.($currentAttendee?->bioFamily?->email ?: '--') }}</div>
+            <div>{{ __('registration.bio-family.telephone').': '.($currentAttendee?->bioFamily?->phone ?: '--') }}</div>
+        </div>
+
+        <div>
+            {{ __('registration.about-host-family-one') }}
+            <div>{{ __('registration.host-family.name').': '.($currentAttendee?->firstHostFamily()?->name ?: '--') }}</div>
+            <div>{{ __('registration.host-family.email').': '.($currentAttendee?->firstHostFamily()?->email ?: '--') }}</div>
+            <div>{{ __('registration.host-family.phone').': '.($currentAttendee?->firstHostFamily()?->phone ?: '--') }}</div>
+            <div>{{ __('registration.host-family.address').': '.($currentAttendee?->firstHostFamily()?->address ?: '--') }}</div>
+        </div>
+
+        <div>
+            {{ __('registration.about-host-family-two') }}
+            <div>{{ __('registration.host-family.name').': '.($currentAttendee?->secondHostFamily()?->name ?: '--') }}</div>
+            <div>{{ __('registration.host-family.email').': '.($currentAttendee?->secondHostFamily()?->email ?: '--') }}</div>
+            <div>{{ __('registration.host-family.phone').': '.($currentAttendee?->secondHostFamily()?->phone ?: '--') }}</div>
+            <div>{{ __('registration.host-family.address').': '.($currentAttendee?->secondHostFamily()?->address ?: '--') }}</div>
+        </div>
+
+        <div>
+            {{ __('registration.about-host-family-three') }}
+            <div>{{ __('registration.host-family.name').': '.($currentAttendee?->thirdHostFamily()?->name ?: '--') }}</div>
+            <div>{{ __('registration.host-family.email').': '.($currentAttendee?->thirdHostFamily()?->email ?: '--') }}</div>
+            <div>{{ __('registration.host-family.phone').': '.($currentAttendee?->thirdHostFamily()?->phone ?: '--') }}</div>
+            <div>{{ __('registration.host-family.address').': '.($currentAttendee?->thirdHostFamily()?->address ?: '--') }}</div>
+        </div>
+    </div>
 </div>
--- tests/Feature/Livewire/RegistrationInfoViewTest.php
+++ tests/Feature/Livewire/RegistrationInfoViewTest.php
@@ -11,7 +11,6 @@ use App\Models\RotaryInfo;
 use App\Models\User;
 use App\Models\YeoInfo;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Arr;
 use Livewire\Livewire;
 
 uses(RefreshDatabase::class);
@@ -43,12 +42,8 @@ it('shows full names of all registered attendees in select', function () {
     Livewire::test('registration-info-view', [
         'attendees' => $attendees,
     ])->assertSee(
-        Arr::flatten(
-            [
-                '<select>',
-                $fullNames,
-                '</select>',
-            ]), escape: false);
+        $fullNames,
+        escape: false);
 });
 
 it('has attendee preselected', function () {
@@ -66,9 +61,159 @@ it('has attendee preselected', function () {
                      ->create();
     $event->attendees()->saveMany($attendees);
 
+    $component = Livewire::test('registration-info-view', [
+        'attendees' => $attendees,
+    ]);
+    $component
+        ->assertOk()
+        ->assertSee($attendees->first()->full_name);
+
+    $currentAttendee = $component->get('currentAttendeeId');
+    expect($currentAttendee)->toBe($attendees->first()->id);
+});
+
+it('shows attributes of currently selected attendee', function () {
+    $attendees = User::factory()->count(2)
+                     ->has(Passport::factory())
+                     ->has(RotaryInfo::factory())
+                     ->has(CounselorInfo::factory(), 'counselor')
+                     ->has(YeoInfo::factory(), 'yeo')
+                     ->has(BioFamily::factory())
+                     ->has(HostFamily::factory()->nth(1))
+                     ->has(HostFamily::factory()->nth(2))
+                     ->has(HostFamily::factory()->nth(3))
+                     ->create();
+    $firstAttendee = $attendees[0];
+    $secondAttendee = $attendees[1];
+
     Livewire::test('registration-info-view', [
         'attendees' => $attendees,
     ])
             ->assertOk()
-            ->assertSee($attendees->first()->full_name);
+            ->set('currentAttendeeId', $firstAttendee->id)
+            ->assertSeeInOrder([
+                __('registration.birthday').': '.$firstAttendee->birthday->translatedFormat('d. F Y'),
+                __('registration.gender.gender').': '.$firstAttendee->gender,
+                __('signup.email').': '.$firstAttendee->email,
+                __('registration.mobile_phone').': '.$firstAttendee->mobile_phone,
+                __('registration.health_issues').': '.$firstAttendee->health_issues,
+
+                __('registration.passport'),
+                __('registration.nationality').': '.$firstAttendee->passport->nationality,
+                __('registration.passport-number').': '.$firstAttendee->passport->passport_number,
+                __('registration.passport-issue-date').': '.$firstAttendee->passport->issue_date,
+                __('registration.passport-expiration-date').': '.$firstAttendee->passport->expiration_date,
+
+                __('registration.about-rotary'),
+                __('registration.rotary.host-club').': '.$firstAttendee->rotaryInfo->host_club,
+                __('registration.rotary.host-district').': '.$firstAttendee->rotaryInfo->host_district,
+                __('registration.rotary.sponsor-club').': '.$firstAttendee->rotaryInfo->sponsor_club,
+                __('registration.rotary.sponsor-district').': '.$firstAttendee->rotaryInfo->sponsor_district,
+
+                __('registration.about-counselor'),
+                __('registration.counselor.name').': '.$firstAttendee->counselor->name,
+                __('registration.counselor.telephone').': '.$firstAttendee->counselor->phone,
+                __('registration.counselor.email').': '.$firstAttendee->counselor->email,
+
+                __('registration.about-yeo'),
+                __('registration.yeo.name').': '.$firstAttendee->yeo->name,
+                __('registration.yeo.telephone').': '.$firstAttendee->yeo->phone,
+                __('registration.yeo.email').': '.$firstAttendee->yeo->email,
+
+                __('registration.about-bio-family'),
+                __('registration.bio-family.parent-one').': '.$firstAttendee->bioFamily->parent_one,
+                __('registration.bio-family.parent-two').': '.$firstAttendee->bioFamily->parent_two,
+                __('registration.bio-family.email').': '.$firstAttendee->bioFamily->email,
+                __('registration.bio-family.telephone').': '.$firstAttendee->bioFamily->phone,
+
+                __('registration.about-host-family-one'),
+                __('registration.host-family.name').': '.$firstAttendee->firstHostFamily()->name,
+                __('registration.host-family.email').': '.$firstAttendee->firstHostFamily()->email,
+                __('registration.host-family.phone').': '.$firstAttendee->firstHostFamily()->phone,
+                __('registration.host-family.address').': '.$firstAttendee->firstHostFamily()->address,
+
+                __('registration.about-host-family-two'),
+                __('registration.host-family.name').': '.$firstAttendee->secondHostFamily()->name,
+                __('registration.host-family.email').': '.$firstAttendee->secondHostFamily()->email,
+                __('registration.host-family.phone').': '.$firstAttendee->secondHostFamily()->phone,
+                __('registration.host-family.address').': '.$firstAttendee->secondHostFamily()->address,
+
+                __('registration.about-host-family-three'),
+                __('registration.host-family.name').': '.$firstAttendee->thirdHostFamily()->name,
+                __('registration.host-family.email').': '.$firstAttendee->thirdHostFamily()->email,
+                __('registration.host-family.phone').': '.$firstAttendee->thirdHostFamily()->phone,
+                __('registration.host-family.address').': '.$firstAttendee->thirdHostFamily()->address,
+
+            ]);
+});
+
+it('can handle user properties being null', function () {
+    $attendee = User::factory()
+                    ->state([
+                        'birthday' => null,
+                        'gender' => null,
+                        'mobile_phone' => null,
+                        'health_issues' => null,
+                    ])
+                    ->make();
+    Livewire::test('registration-info-view', [
+        'attendees' => [$attendee],
+    ])->assertOk()->assertSee([
+        __('registration.birthday').': --',
+        __('registration.gender.gender').': --',
+        __('registration.mobile_phone').': --',
+        __('registration.health_issues').': --',
+        __('registration.passport-number').': --',
+        __('registration.nationality').': --',
+        __('registration.passport-issue-date').': --',
+        __('registration.passport-expiration-date').': --',
+        __('registration.rotary.host-club').': --',
+        __('registration.rotary.host-district').': --',
+        __('registration.rotary.sponsor-club').': --',
+        __('registration.rotary.sponsor-district').': --',
+        __('registration.counselor.name').': --',
+        __('registration.counselor.telephone').': --',
+        __('registration.counselor.email').': --',
+        __('registration.yeo.name').': --',
+        __('registration.yeo.telephone').': --',
+        __('registration.yeo.email').': --',
+        __('registration.bio-family.parent-one').': --',
+        __('registration.bio-family.parent-two').': --',
+        __('registration.bio-family.email').': --',
+        __('registration.bio-family.telephone').': --',
+        __('registration.host-family.name').': --',
+        __('registration.host-family.email').': --',
+        __('registration.host-family.phone').': --',
+        __('registration.host-family.address').': --',
+
+    ], escape: false);
+});
+
+it('has current attendee wired', function () {
+    $a = User::factory()->create();
+    Livewire::test('registration-info-view', [
+        'attendees' => [$a],
+    ])
+            ->assertOk()
+            ->assertPropertyWired('currentAttendeeId');
+});
+
+it('updates the current attendee when updating the current attendee id', function () {
+    $attendees = User::factory()->count(2)->create();
+    $firstAttendee = $attendees[0];
+    $secondAttendee = $attendees[1];
+
+    $component = Livewire::test('registration-info-view', [
+        'attendees' => $attendees,
+    ]);
+    $component
+        ->assertOk()
+        ->set('currentAttendeeId', $firstAttendee->id);
+
+    expect($component->get('currentAttendee'))
+        ->toBeSameEntityAs($firstAttendee);
+
+    $component->set('currentAttendeeId', $secondAttendee->id);
+    expect($component->get('currentAttendee'))
+        ->toBeSameEntityAs($secondAttendee);
 });
