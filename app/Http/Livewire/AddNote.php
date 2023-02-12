816fb77f3f4a643f1c482acaf60cb636b97f9031
Paul Schuberth <github@pschuberth.de>
2023-02-12 23:46:27 +0100

    fix phpstan findings

--- app/Http/Livewire/AddNote.php
+++ app/Http/Livewire/AddNote.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\AdditionalInfo;
 use App\Models\User;
 use App\Policies\NotePolicy;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
@@ -13,19 +17,23 @@ class AddNote extends Component
 
     public ?string $note = null;
 
-    public function render()
+    public function render(): Factory|View|Application
     {
         $this->note = $this->attendee->additionalInfo?->note;
 
         return view('livewire.add-note');
     }
 
-    public function updatedNote(string $note)
+    public function updatedNote(string $note): void
     {
         $allowedToCreateNote = (new NotePolicy())->createNote(Auth::user())->allowed();
         if ($allowedToCreateNote) {
-            $this->attendee->additionalInfo->note = $note;
-            $this->attendee->additionalInfo->save();
+            $info = $this->attendee->additionalInfo;
+            if ($info == null) {
+                $info = new AdditionalInfo();
+            }
+            $info->note = $note;
+            $this->attendee->additionalInfo()->save($info);
         }
     }
 }
