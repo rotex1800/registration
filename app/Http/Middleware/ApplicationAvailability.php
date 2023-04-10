1f4f40d616b5fcac98e72652483a5b8cc905e971
Paul Schuberth <github@pschuberth.de>
2023-04-10 20:36:51 +0200

    Create middleware redirecting to rotex page when registration is closed

---
+++ app/Http/Middleware/ApplicationAvailability.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Carbon\Carbon;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApplicationAvailability
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(Request): (Response) $next
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Carbon::now()->isAfter('2023-08-10T00:00:00')) {
+            return $next($request);
+        } else {
+            return redirect('https://rotex1800.de');
+        }
+    }
+}
