31993cc0b0771e8f61024b59c472c8172bad8526
Paul Schuberth <github@pschuberth.de>
2023-09-09 18:13:54 +0200

    chore: allow to set multiple roles to an event at the same time

--- tests/Feature/Console/Commands/CreateEventTest.php
+++ tests/Feature/Console/Commands/CreateEventTest.php
@@ -4,29 +4,41 @@ use App\Models\Role;
 use App\Models\User;
 use App\Policies\EventPolicy;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+
 use function Pest\Laravel\assertDatabaseCount;
 
 uses(RefreshDatabase::class);
 
 it('creates a new event', function () {
     // Arrange
+    Role::factory()->participant()->create();
+    Role::factory()->rotex()->create();
 
     // Act
     $this->artisan('registration:create')
-         ->expectsQuestion('What is the name of the event?', 'EuropaTour 2024')
-         ->expectsQuestion("When does it start?", '2024-03-25')
-         ->expectsQuestion('When does it end?', '2024-04-14')
-         ->expectsQuestion('Who is the target audience of the event?', 'participant')
-         ->assertOk();
+        ->expectsQuestion('What is the name of the event?', 'EuropaTour 2024')
+        ->expectsQuestion('When does it start?', '2024-03-25')
+        ->expectsQuestion('When does it end?', '2024-04-14')
+        ->expectsChoice(
+            'Who is the target audience of the event?',
+            ['rotex', 'participant'],
+            ['participant', 'rotex']
+        )
+        ->assertOk();
 
     // Assert
     assertDatabaseCount('events', 1);
     $actual = App\Models\Event::all()->first();
     $participant = User::factory()
-                       ->create();
+        ->create();
     $participant->giveRole('participant');
+    $rotex = User::factory()->create();
+    $rotex->giveRole('rotex');
 
-    expect((new EventPolicy())->show($participant, $actual))
+    $eventPolicy = new EventPolicy();
+    expect($eventPolicy->show($participant, $actual))
+        ->toBeAllowed()
+        ->and($eventPolicy->show($rotex, $actual))
         ->toBeAllowed()
         ->and($actual)
         ->name->toBe('EuropaTour 2024')
