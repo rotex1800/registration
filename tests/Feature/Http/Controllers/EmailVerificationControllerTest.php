e89549b4523c05a85c5704d01ee878093cc82886
Paul Schuberth <github@pschuberth.de>
2023-09-16 00:15:07 +0200

    fix: blank screen after requesting verification mail

--- tests/Feature/Http/Controllers/EmailVerificationControllerTest.php
+++ tests/Feature/Http/Controllers/EmailVerificationControllerTest.php
@@ -54,12 +54,26 @@ it('sends verification email', function () {
     $user = User::factory()->unverified()->create();
 
     $this->actingAs($user)
-        ->post(route('verification.send'));
+        ->post(route('verification.send'))
+        ->assertRedirect(route('verification.notice'));
 
     Notification::assertSentTo($user, VerifyEmail::class);
 });
 
 it('requires authentication to request verification email', function () {
     $this->post(route('verification.send'))
+        ->assertRedirect(route('login'));
+});
+
+it('shows verification hint for unverified user', function () {
+    $user = User::factory()->unverified()->create();
+
+    $this->actingAs($user)
+        ->get(route('home'))
+        ->assertRedirect(route('verification.notice'));
+});
+
+it('requires authentication to see verification hint', function () {
+    $this->get(route('verification.notice'))
         ->assertRedirect(route('login'));
 });
